263137b36716610b1a3016ab921e00a52edd5774
Аяз Аюпов <ayaz.ayupov@gmail.com>
2019-01-21 16:08:54 +0300

    added struct type Reverser

---
+++ reverser/reverser.go
@@ -0,0 +1,41 @@
+package reverser
+
+import (
+	"fmt"
+	"github.com/Ayupov-Ayaz/reverse_db_to_graphql/db"
+	"github.com/Ayupov-Ayaz/reverse_db_to_graphql/model"
+)
+/**
+  Structure for reversing DataBase structure to GraphQl structure
+ */
+type Reverser struct {
+	OutputFileName string
+	Tables []string
+}
+
+func NewReverser(tables *[]string) *Reverser {
+	return &Reverser{
+		Tables: *tables,
+	}
+}
+
+func (r *Reverser) Reverse(db *db.DB) error {
+	var tableStructs = make([]model.Table, 0)
+
+	// Получаем структуры таблиц
+	for _, table := range r.Tables {
+		tableStruct, err := getTableStruct(table, db)
+		if err != nil {
+			panic(err)
+		}
+		tableStructs = append(tableStructs, *tableStruct)
+	}
+
+	// TODO: передавать в шаблон
+	for _,table := range tableStructs {
+		for _, field := range *table.Fields {
+			fmt.Println(field.Name)
+		}
+	}
+	return nil
+}
