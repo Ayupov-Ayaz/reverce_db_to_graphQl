390b528b0d622019fd9597d653a73fe7354c7276
Аяз Аюпов <ayaz.ayupov@gmail.com>
2019-01-21 16:14:15 +0300

    starting reverse() for all tables from daemon

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -1,8 +1,8 @@
 package daemon
 
 import (
-	"fmt"
 	"github.com/Ayupov-Ayaz/reverse_db_to_graphql/db"
+	"github.com/Ayupov-Ayaz/reverse_db_to_graphql/reverser"
 	"log"
 )
 
@@ -10,11 +10,14 @@ type Config struct {
 	Db *db.Config
 }
 
-func Run(cfg *Config) error {
+func Run(cfg *Config, tables *[]string) error {
 	db, err := db.InitDB(cfg.Db)
 	if err != nil {
 		log.Println("ERROR| Initialization db: \n %s", err.Error())
 	}
-	fmt.Println(db)
+	// scanning
+	rev := reverser.NewReverser( tables)
+	rev.Reverse(db)
+
 	return nil
 }
