8e52530025d461374a41743af91e4cf22e8b0e40
Аяз Аюпов <ayaz.ayupov@gmail.com>
2019-01-21 17:28:26 +0300

    tmp

--- reverser/reverser.go
+++ reverser/reverser.go
@@ -1,9 +1,12 @@
 package reverser
 
 import (
-	"fmt"
+	"bytes"
 	"github.com/Ayupov-Ayaz/reverse_db_to_graphql/db"
 	"github.com/Ayupov-Ayaz/reverse_db_to_graphql/model"
+	"io/ioutil"
+	"os"
+	"text/template"
 )
 /**
   Structure for reversing DataBase structure to GraphQl structure
@@ -32,10 +35,29 @@ func (r *Reverser) Reverse(db *db.DB) error {
 	}
 
 	// TODO: передавать в шаблон
-	for _,table := range tableStructs {
-		for _, field := range *table.Fields {
-			fmt.Println(field.Name)
-		}
-	}
+	sendToTemplate(&tableStructs)
+
+
 	return nil
+}
+
+func sendToTemplate(tables *[]model.Table) {
+	// Открытваем(Создаем) файл для записи с именем таблицы
+
+	//fo, err := os.Create("result/1.txt")
+	//if err != nil { panic(err) }
+	//
+	//defer func() {
+	//	if err := fo.Close(); err != nil { panic(err) }
+	//}()
+
+	buff := bytes.NewBufferString("")
+
+	template, err := template.New("Table").Parse("/templates/table.txt")
+	if err != nil {
+		panic(err)
+	}
+	if err := template.Execute(buff, tables); err != nil { panic(err) }
+	if err := ioutil.WriteFile("result/1.txt", buff.Bytes(), os.ModePerm); err != nil { panic(err) }
+	//err = tmpl.ExecuteTemplate(write, table)
 }
