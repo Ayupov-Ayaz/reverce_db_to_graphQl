ed6d18d0d0abe2fe2a42f7c2e713e48d5ecaa754
Аяз Аюпов <ayaz.ayupov@gmail.com>
2019-01-22 13:34:51 +0300

    changed model Field, added foreign

--- model/Table.go
+++ model/Table.go
@@ -14,7 +14,8 @@ type Table struct {
 type Field struct {
 	Name string `db:"name"`
 	Type string `db:"type"`
-	IsPrimary bool `db:"primary_key"`
+	IsPrimary bool `db:"is_primary"`
+	IsForeign bool `db:"is_foreign"`
 	IsNullable bool `db:"is_nullable"`
 	MaxLength int `db:"max_length"`
 }
