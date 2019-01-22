0cc2638a44a15e6de636ef0fa80854d450a702f6
Аяз Аюпов <ayaz.ayupov@gmail.com>
2019-01-22 13:34:14 +0300

    changed sql: fix getting primary_key, added getting foreign_key

--- reverser/commands.go
+++ reverser/commands.go
@@ -15,23 +15,31 @@ func getTableStruct(tableName string, db *db.DB)  (table *model.Table, err error
 	}
 
 	query := `
-			 select distinct 
-			 ISC.COLUMN_NAME                               as name
-			 ,ISC.DATA_TYPE   							   as type
-			 ,isnull(ISC.CHARACTER_MAXIMUM_LENGTH, 0)      as max_length
-			 ,case when lower(ISC.IS_NULLABLE) = 'yes' 
-					then  1 else 0 end 	       			   as is_nullable
-			 ,case when 
-				 ISC.COLUMN_NAME = ISKCU.COLUMN_NAME 
-				 then 1 else 0 end 					       as primary_key
+		select distinct
+     			   t_c.COLUMN_NAME                             					      as name
+     			  , t_c.DATA_TYPE                                                     as type
+     			  , isnull(t_c.CHARACTER_MAXIMUM_LENGTH, 0)					          as max_length
+			  
+     			  , case when lower(t_c.IS_NULLABLE) = 'yes' then  1 else 0 end       as is_nullable
+			  
+     			  , case when p_keys.CONSTRAINT_TYPE is not null then 1 else 0 end    as is_primary
+			  
+     			  , case when f_keys.CONSTRAINT_TYPE is not null then 1 else 0 end    as is_foreign
 
-			 from INFORMATION_SCHEMA.COLUMNS ISC
-			 left join INFORMATION_SCHEMA.KEY_COLUMN_USAGE ISKCU on
-			 ISKCU.TABLE_NAME =  ISC.TABLE_NAME
-			 	   and  ISKCU.TABLE_CATALOG = ISC.TABLE_CATALOG
-			 	   and  ISKCU.TABLE_SCHEMA = ISC.TABLE_SCHEMA
-			 
-			 where ISC.TABLE_NAME = ? `
+        from INFORMATION_SCHEMA.COLUMNS t_c
+	   
+        left join INFORMATION_SCHEMA.KEY_COLUMN_USAGE all_keys on all_keys.TABLE_NAME =  t_c.TABLE_NAME
+                                                              and t_c.COLUMN_NAME = all_keys.COLUMN_NAME
+
+        left join INFORMATION_SCHEMA.TABLE_CONSTRAINTS AS p_keys on p_keys.TABLE_NAME = all_keys.TABLE_NAME
+                                                                and p_keys.CONSTRAINT_NAME = all_keys.CONSTRAINT_NAME
+                                                                and p_keys.CONSTRAINT_TYPE = 'PRIMARY KEY'
+
+        left join INFORMATION_SCHEMA.TABLE_CONSTRAINTS AS f_keys on f_keys.TABLE_NAME = all_keys.TABLE_NAME
+                                                                and f_keys.CONSTRAINT_NAME = all_keys.CONSTRAINT_NAME
+                                                                and f_keys.CONSTRAINT_TYPE = 'FOREIGN KEY'
+
+	    where t_c.TABLE_NAME = ? `
 
 	if  err := db.Select(fields, query, tableName); err != nil {
 		return nil, err
