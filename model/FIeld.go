d1b63a3fc067c1fd624b094467403cb6ea1fa4cd
Аяз Аюпов <ayaz.ayupov@gmail.com>
2019-01-22 16:19:55 +0300

    moving model Field to self files

--- model/FIeld.go
+++ model/FIeld.go
@@ -6,6 +6,15 @@ import (
 	"strconv"
 )
 
+type Field struct {
+	Name string `db:"name"`
+	Type string `db:"type"`
+	IsPrimary bool `db:"is_primary"`
+	IsForeign bool `db:"is_foreign"`
+	IsNullable bool `db:"is_nullable"`
+	MaxLength int `db:"max_length"`
+}
+
 func (f *Field) IsPrimaryKey() string {
 	if f.IsPrimary {
 		return "@primary"
--- model/Table.go
+++ model/Table.go
@@ -7,11 +7,3 @@ type Table struct {
 	ForeignKeys *[]ForeignKey
 }
 
-type Field struct {
-	Name string `db:"name"`
-	Type string `db:"type"`
-	IsPrimary bool `db:"is_primary"`
-	IsForeign bool `db:"is_foreign"`
-	IsNullable bool `db:"is_nullable"`
-	MaxLength int `db:"max_length"`
-}
