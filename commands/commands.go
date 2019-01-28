b4f875828b2ce0a0ccc9eb67b9ea080d6d28c222
Аяз Аюпов <ayaz.ayupov@gmail.com>
2019-01-28 13:13:50 +0300

    added DbCommander interface for work with db

---
+++ commands/commands.go
@@ -0,0 +1,26 @@
+package commands
+
+import (
+	"github.com/Ayupov-Ayaz/reverse_db_to_graphql/commands/impl"
+	"github.com/Ayupov-Ayaz/reverse_db_to_graphql/db"
+	"github.com/Ayupov-Ayaz/reverse_db_to_graphql/model"
+	"log"
+	"os"
+)
+
+type DbCommander interface {
+	GetTableStruct(tableName string, db *db.DB) (table *model.Table)
+	GetForeignKeys(tableName string, db *db.DB) []*model.ForeignKey
+}
+
+func GetDbCommander(cfg *db.Config) DbCommander{
+	switch cfg.Driver {
+	case db.DriverMSSQL:
+		return impl.NewMssqlCommands()
+
+	default:
+		log.Printf("| ERROR | Для %s нет реализаций", cfg.Driver)
+		os.Exit(-1)
+		return nil
+	}
+}
