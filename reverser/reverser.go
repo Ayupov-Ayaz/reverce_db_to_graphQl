c6b080e6b57ecfe25460ba60ff886b826b6157ab
Аяз Аюпов <ayaz.ayupov@gmail.com>
2019-01-29 16:10:18 +0300

    function for work with template moved in template.go file

--- reverser/reverser.go
+++ reverser/reverser.go
@@ -5,7 +5,6 @@ import (
 	"github.com/Ayupov-Ayaz/reverse_db_to_graphql/commands"
 	"github.com/Ayupov-Ayaz/reverse_db_to_graphql/db"
 	"github.com/Ayupov-Ayaz/reverse_db_to_graphql/model"
-	"html/template"
 	"log"
 	"os"
 )
@@ -50,46 +49,6 @@ func (r *Reverser) Reverse(db *db.DB, com commands.DbCommander, flags map[string
 	SpecialTypeDefinition(tableCollection, relations)
 	// отправляем в шаблон
 	sendToTemplate(tableStructs)
-}
-
-/**
-	Выгружает данные в шаблон и создает в папке results graphql структуры
- */
-func sendToTemplate(tables []*model.Table) {
-
-	field := model.Field{}
-	funcMap :=  template.FuncMap{
-		"IsPrimaryKey"      : field.IsPrimaryKey,
-		"IsNullableField"	: field.IsNullableField,
-		"GetValidate"       : field.GetValidate,
-		"GetGraphQlType"	: field.GetGraphQlType,
-		"GetForeignType" 	: model.GetForeignType,
-	}
-
-	t, err := template.New("tables").Funcs(funcMap).Parse(getTemplateStruct())
-	if err != nil {
-		log.Printf("| SYS.ERROR | Не удалось создать шаблон \n")
-		panic(err)
-	}
-	for _,table := range tables {
-		fileName := "results/" + table.Name + ".graphql"
-		fo, err := os.Create(fileName)
-		if err != nil {
-			log.Printf("| SYS.ERROR | Не удалось открыть файл %s, \n", fileName)
-			panic(err)
-		}
-
-		if err := t.Execute(fo, table); err != nil {
-			log.Printf("| SYS.ERROR | При попытке записать в файл %s структуру таблицы %s произошла ошибка:\n",
-			fileName, table.Name)
-			panic(err)
-		}
-
-		if err := fo.Close(); err != nil {
-			log.Printf("| SYS.ERROR | Не удалось закрыть файл %s \n", fileName)
-		}
-	}
-
 }
 
 /**
--- reverser/templateStruct.go
+++
@@ -1,9 +0,0 @@
-package reverser
-
-func getTemplateStruct() string {
-	return `
-	type {{.Name}} { {{range.Fields}}
-		{{.Name}}: {{if .IsForeign}}{{GetForeignType .}}{{else}}{{.GetGraphQlType}}{{end}}{{.IsNullableField}} {{.IsPrimaryKey}} {{.GetValidate}}{{end}}
-	}
-`
-}
