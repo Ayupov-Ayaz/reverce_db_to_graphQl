bab8b360986d9a01769e1f3605bf1b8176ed1f2e
Аяз Аюпов <ayaz.ayupov@gmail.com>
2019-01-30 10:15:40 +0300

    print directives

--- model/Table.go
+++ model/Table.go
@@ -9,6 +9,16 @@ type Table struct {
 	Directives    []string
 }
 
+func GetTableDirectiveByTable(t Table) string {
+	if len(t.Directives) == 0 {
+		return ""
+	}
+	str := ""
+	for _, directive := range t.Directives {
+		str += " "+directive
+	}
+	return str
+}
 
 func GetTableDirectiveCollection() map[string]string {
 	return map[string]string{
